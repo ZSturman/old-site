96376fe2f29339e0699c2ff52c555517fe9b3bc8
Zachary Sturman <100522031+ZSturman@users.noreply.github.com>
2022-04-24 08:36:31 -0600

    index update

--- js/main.js
+++ js/main.js
@@ -1,11 +1,22 @@
-const modal = document.querySelector('.modal');
-const openModal = document.querySelector('.open-button');
-const closeModal = document.querySelector('.close-button');
+const modal1 = document.querySelector('.modal1');
+const openModal1 = document.querySelector('.open-button1');
+const closeModal1 = document.querySelector('.close-button1');
 
-openModal.addEventListener('click', () =>{
-    modal.showModal();
+openModal1.addEventListener('click', () =>{
+    modal1.showModal();
 })
 
-closeModal.addEventListener('click', () => {
-    modal.close();
+closeModal1.addEventListener('click', () => {
+    modal1.close();
+})
+const modal2 = document.querySelector('.modal2');
+const openModal2 = document.querySelector('.open-button2');
+const closeModal2 = document.querySelector('.close-button2');
+
+openModal2.addEventListener('click', () =>{
+    modal2.showModal();
+})
+
+closeModal2.addEventListener('click', () => {
+    modal2.close();
 })
